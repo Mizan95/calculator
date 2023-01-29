96359de6cca48d255ec4cc8d6dca4a227d1a89fd
Mizan_thinkpadX260_Ubuntu <mizanur9522@outlook.com>
2023-01-29 19:32:00 +0000

    Complete Clear button and Delete button functionality

--- script.js
+++ script.js
@@ -4,28 +4,31 @@ const numbers = document.querySelectorAll(".numbers");
 
 numbers.forEach(number => {
     number.addEventListener('click', (e) => {
-        setNumbers(e); 
+        setNumbers(e);
         displayNumbers();
+        // if (operandOne) {
+        //     operandTwo = screenNumbers.join("");
+        // }
     })
 })
 
+
 let screenNumbers = [];
-screen.textContent = 0;
 function setNumbers(button) {
     screenNumbers.push(button.target.textContent);
 }
 
+screen.textContent = 0; 
 function displayNumbers() {
     screen.textContent = screenNumbers.join("");
 }
 
 const operatorButtons = document.querySelectorAll(".operators");
-let operandOne = "";
+let operandOne;
 let operator;
-
 operatorButtons.forEach(button => {
     button.addEventListener('click', (e) => {
-        operandOne = screenNumbers.join("");
+        operandOne = screen.textContent;
         operator = e.target.textContent;
         clearNumbers();
     })
@@ -33,8 +36,10 @@ operatorButtons.forEach(button => {
 
 function clearNumbers() {
     screenNumbers = [];
+    screen.textContent = "";
 }
 
+// Perform operation and display result on screen
 const equals = document.querySelector(".equals");
 let operandTwo;
 let result;
@@ -43,6 +48,30 @@ equals.addEventListener('click', () => {
     result = operate(operator, +operandOne, +operandTwo);
     screen.textContent = result;
 })
+
+// Clear all functionality
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', clearAll);
+function clearAll() {
+    operandOne = 0;
+    operator = "undefined";
+    operandTwo = 0;
+    screenNumbers = [];
+    screen.textContent = 0;
+}
+
+// Clear last number functionality
+const deleteButton = document.querySelector('.delete');
+deleteButton.addEventListener('click', () => {
+    displayNumbers(deleteLastNumber());
+});
+function deleteLastNumber() {
+    screenNumbers.pop();
+    return screenNumbers.join("");
+}
+
+
+
 
 
 //  operation functions
